37c3ca8fd3415120f4a716844bd3c920d9ef1991
snipered2003 <snipered@hotmail.com>
2020-10-22 12:08:31 +0100

    Subscriptions nuget (#72)
    * Get's content from api
    
    * subscriptions
    
    * FAC working

---
+++ Enums/Contracts/IWebhooksService.cs
@@ -0,0 +1,17 @@
+﻿using DFC.App.FindACourse.Data.Enums;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DFC.App.FindACourse.Data.Contracts
+{
+    public interface IWebhooksService
+    {
+        Task<HttpStatusCode> DeleteContentItemAsync(Guid contentItemId);
+
+        Task<HttpStatusCode> ProcessContentAsync(Guid sharedContentId, CancellationToken stoppingToken);
+
+        Task<HttpStatusCode> ProcessMessageAsync(WebhookCacheOperation webhookCacheOperation, Guid eventId, Guid sharedContentId, string apiEndpoint);
+    }
+}
---
+++ Enums/Models/EventGridEventData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DFC.App.FindACourse.Data.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class EventGridEventData
+    {
+        public string? Api { get; set; }
+
+        public string? ItemId { get; set; }
+
+        public string? VersionId { get; set; }
+
+        public string? DisplayText { get; set; }
+
+        public string? Author { get; set; }
+    }
+}
