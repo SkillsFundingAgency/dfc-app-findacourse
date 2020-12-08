3f8c1c59ff948a05e64ab0b734f9b1b8e3304ef4
James Barnes <james@testtrends.co.uk>
2020-12-08 14:02:47 +0000

    Added a single test

---
+++ DFC.App.FindACourse.UI.FunctionalTests/Pages/FindACourseLandingPage.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="FindACourseLandingPage.cs" company="National Careers Service">
+// Copyright (c) National Careers Service. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using DFC.App.FindACourse.UI.FunctionalTests.Model;
+using DFC.TestAutomation.UI.Extension;
+using System;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.FindACourse.UI.FunctionalTests.Pages
+{
+    internal class FindACourseLandingPage
+    {
+        public FindACourseLandingPage(ScenarioContext context)
+        {
+            this.Context = context;
+
+            if (this.Context == null)
+            {
+                throw new NullReferenceException("The scenario context is null. The contact us home page cannot be initialised.");
+            }
+        }
+
+        private ScenarioContext Context { get; set; }
+
+        public FindACourseLandingPage NavigateToFindACoursePage()
+        {
+            this.Context.GetWebDriver().Url = this.Context.GetSettingsLibrary<AppSettings>().AppSettings.AppBaseUrl.ToString();
+            return this;
+        }
+    }
+}
---
+++ DFC.App.FindACourse.UI.FunctionalTests/StepDefinitions/FormSteps.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="FormSteps.cs" company="National Careers Service">
+// Copyright (c) National Careers Service. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using DFC.App.FindACourse.UI.FunctionalTests.Model;
+using DFC.TestAutomation.UI.Extension;
+using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.FindACourse.UI.FunctionalTests.StepDefinitions
+{
+    [Binding]
+    internal class FormSteps
+    {
+        public FormSteps(ScenarioContext context)
+        {
+            this.Context = context;
+        }
+
+        private ScenarioContext Context { get; set; }
+
+        [When(@"I enter the search term (.*) in the search box")]
+        public void WhenIEnterTheSearchTermInTheSearchBox(string text)
+        {
+            var searchBoxes = this.Context.GetWebDriver().FindElements(By.Id("search-input"));
+
+            if (!searchBoxes.Count.Equals(1))
+            {
+                throw new NotFoundException($"Unable to perform the step: {this.Context.StepContext.StepInfo.Text}. The appropriate search box could not be located.");
+            }
+
+            var searchBox = searchBoxes[0];
+            searchBox.SendKeys(text);
+        }
+
+        [When(@"I click the search button")]
+        public void WhenIClickTheSearchButton()
+        {
+            var searchBoxes = this.Context.GetWebDriver().FindElements(By.Id("search-button"));
+
+            if (!searchBoxes.Count.Equals(1))
+            {
+                throw new NotFoundException($"Unable to perform the step: {this.Context.StepContext.StepInfo.Text}. The appropriate search button could not be located.");
+            }
+
+            searchBoxes[0].Click();
+        }
+    }
+}
