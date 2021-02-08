da7739b8229bc7e868d1fddc35d136156cd2f90b
Ian Crisp <icrisp@program-systems.co.uk>
2021-02-08 13:58:10 +0000

    Renamed parameter following Sonar cloud guidance

--- DFC.App.FindACourse/Helpers/IViewHelper.cs
+++ DFC.App.FindACourse/Helpers/IViewHelper.cs
@@ -5,6 +5,6 @@ namespace DFC.App.FindACourse.Helpers
 {
     public interface IViewHelper
     {
-        Task<string> RenderViewAsync<TModel>(Controller controller, string viewName, TModel model, bool partial = false);
+        Task<string> RenderViewAsync<TModel>(Controller controller, string viewName, TModel model, bool isPartialView = false);
     }
 }
--- DFC.App.FindACourse/Helpers/ViewHelper.cs
+++ DFC.App.FindACourse/Helpers/ViewHelper.cs
@@ -12,7 +12,7 @@ namespace DFC.App.FindACourse.Helpers
     [ExcludeFromCodeCoverage]
     public class ViewHelper : IViewHelper
     {
-        public async Task<string> RenderViewAsync<TModel>(Controller controller, string viewName, TModel model, bool partial = false)
+        public async Task<string> RenderViewAsync<TModel>(Controller controller, string viewName, TModel model, bool isPartialView = false)
         {
             _ = controller ?? throw new ArgumentNullException(nameof(controller));
 
@@ -20,7 +20,7 @@ namespace DFC.App.FindACourse.Helpers
 
             using var writer = new StringWriter();
             IViewEngine viewEngine = controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
-            ViewEngineResult viewResult = viewEngine.GetView(viewName, viewName, !partial);
+            ViewEngineResult viewResult = viewEngine.GetView(viewName, viewName, !isPartialView);
 
             if (!viewResult.Success)
             {
