68fb2440e32dd3a3eed92105201e976034f92067
gurney <Gurney@as-tech.co.uk>
2021-02-16 11:35:54 +0000

    Replaced carrigae returns with <br> for text sections

---
+++ DFC.App.FindACourse.UnitTests/HTMLHelpers/HtmlFormatTests.cs
@@ -0,0 +1,31 @@
+﻿using DFC.App.FindACourse.Helpers;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace DFC.App.FindACourse.UnitTests
+{
+    public class HtmlFormatTests
+    {
+        [Theory]
+        [InlineData ("aa\r\nbb", "aa<br/>bb")]
+        [InlineData("aa\r\n \r\nbb", "aa<br/> <br/>bb")]
+        public void HtmlFormatsCorrectly(string input, string expectedOutput)
+        {
+            //Act
+            var fakeHelper = A.Fake<IHtmlHelper>();
+
+            var output = HTMLHelpers.HtmlFormat(fakeHelper, input);
+
+            //Asserts
+            output.ToString().Should().Be(expectedOutput);
+        }
+
+    }
+}
---
+++ DFC.App.FindACourse/Helpers/HTMLHelpers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DFC.App.FindACourse.Helpers
+{
+    public static class HTMLHelpers
+    {
+        public static IHtmlContent HtmlFormat(this IHtmlHelper htmlHelper, string content)
+        {
+            if (content == null)
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            return new HtmlString(content.Replace(System.Environment.NewLine, "<br/>"));
+        }
+    }
+}
