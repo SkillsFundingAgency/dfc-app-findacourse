e1aee4a1ca0ec2df3a8146c4af011a86a04c3a20
Stephen Yates <SYatesSoftware@gmail.com>
2021-03-02 15:52:39 +0000

    accordion changes for sonarcloud

---
+++ DFC.App.FindACourse/ViewModels/AccordionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DFC.App.FindACourse.ViewModels
+{
+    [ExcludeFromCodeCoverage]
+    public class AccordionViewModel
+    {
+        public string SectionName { get; set; }
+
+        public string Title { get; set; }
+
+        public string SubText { get; set; }
+
+        public string Description { get; set; }
+    }
+}
