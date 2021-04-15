0900c028ee6912de3f3cad57d8c0d6de2563d041
gurney <Gurney@as-tech.co.uk>
2021-04-15 15:04:11 +0100

    Remove unused namespaces

--- Enums/Models/LocationCoordinates.cs
+++ Enums/Models/LocationCoordinates.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace DFC.App.FindACourse.Data.Models
 {
+    [ExcludeFromCodeCoverage]
     public class LocationCoordinates
     {
         public bool AreValid { get; set; }
