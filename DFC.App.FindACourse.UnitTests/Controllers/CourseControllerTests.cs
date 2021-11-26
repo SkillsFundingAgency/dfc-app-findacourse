cdf8dd573092e15cf895373d144671f9971185dd
Ian Crisp <icrisp@program-systems.co.uk>
2021-11-26 15:09:02 +0000

    updated nugets

--- DFC.App.FindACourse.UnitTests/Controllers/CourseControllerTests.cs
+++ DFC.App.FindACourse.UnitTests/Controllers/CourseControllerTests.cs
@@ -438,7 +438,7 @@ namespace DFC.App.FindACourse.UnitTests.Controllers
             Func<Task> act = async () => await controller.AjaxChanged(null).ConfigureAwait(false);
 
             // assert
-            act.Should().Throw<ArgumentNullException>();
+            act.Should().ThrowAsync<ArgumentNullException>();
             controller.Dispose();
         }
 
@@ -660,7 +660,7 @@ namespace DFC.App.FindACourse.UnitTests.Controllers
             Func<Task> act = async () => await controller.IsValidPostcode("\"\"").ConfigureAwait(false);
 
             // assert
-            act.Should().Throw<ArgumentNullException>();
+            act.Should().ThrowAsync<ArgumentNullException>();
 
             controller.Dispose();
         }
