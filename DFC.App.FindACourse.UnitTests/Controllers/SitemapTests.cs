ac54e94777e6bfbc685fe9f2f5e4414bc48aadbe
ryanbarlowmethods <ryan.barlow@methods.co.uk>
2022-03-03 15:51:35 +0000

    Added sitemap with two paths

---
+++ DFC.App.FindACourse.UnitTests/Controllers/SitemapTests.cs
@@ -0,0 +1,25 @@
+﻿using DFC.App.FindACourse.Controllers;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace DFC.App.FindACourse.UnitTests.Controllers
+{
+    public class SitemapTests
+    {
+        private readonly SitemapController controller;
+
+        public SitemapTests()
+        {
+            controller = new SitemapController(A.Fake<ILogger<SitemapController>>());
+        }
+
+        [Fact]
+        public void SitemapGeneratedSuccesfully()
+        {
+            var actionResponse = controller.SitemapView();
+            Assert.IsType<ContentResult>(actionResponse);
+        }
+    }
+}
